4f6c1decba0a44e45f142af352431dae8752a283
Geo Perez <geovanni.perez@gmail.com>
2015-04-25 17:52:57 -0500

    Styling

---
+++ assets/js/fbconversion.js
@@ -0,0 +1,14 @@
+
+        (function () {
+            var _fbq = window._fbq || (window._fbq = []);
+            if (!_fbq.loaded) {
+                var fbds = document.createElement('script');
+                fbds.async = true;
+                fbds.src = '//connect.facebook.net/en_US/fbds.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(fbds, s);
+                _fbq.loaded = true;
+            }
+        })();
+        window._fbq = window._fbq || [];
+        window._fbq.push(['track', '6020837596519', { 'value': '0.00', 'currency': 'MXN' }]);
